2cbda90e5ef7c7b5d437c6e44c7e1a440709794d
MaiCong <i@maicong.me>
2017-12-22 17:13:10 +0800

    Rebuid

--- assets/build/manifest.php
+++ assets/build/manifest.php
@@ -5,7 +5,7 @@
 */
 class WebpackManifest {
   static $jsFiles = [
-    'assets/build/stay-ab439c8.min.js',
+    'assets/build/stay-0f8543a.min.js',
   ];
   static $cssFiles = [
     'assets/build/stay-d03847d.min.css',
